12d90f3bff515e1cc2e81bea3f2a2e4678f7735a
Bala <balachander.m@freshdesk.com>
2017-02-09 17:08:15 +0530

    Initial Commit

---
+++ PhotoLibPOC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhotoLibPOC
+//
+//  Created by Bala on 09/02/17.
+//  Copyright © 2017 Freshdesk Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PhotoLibPOC/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PhotoLibPOC
+//
+//  Created by Bala on 09/02/17.
+//  Copyright © 2017 Freshdesk Inc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
